92b2b21b2c55a4ffae6363ffc39bd270b6d48365
Daniela Mesa <danielamesa0@gmail.com>
2024-10-29 20:29:33 -0500

    Add raw_movies create table script

--- docker/postgres/init-scripts/init-scripts.sql
+++ docker/postgres/init-scripts/init-scripts.sql
@@ -1,12 +1,15 @@
 CREATE TABLE raw_movies (
     id SERIAL PRIMARY KEY,
-    page_number INT,
-    data JSONB NOT NULL,   
-    source TEXT,  
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
-    file_name TEXT,                   
-    file_size BIGINT,                
-    movies_count INTEGER,             
-    processing_status TEXT,          
-    error_message TEXT           
+    title VARCHAR(255) NOT NULL,
+    overview TEXT,
+    release_date DATE,
+    vote_average NUMERIC(3, 1),
+    vote_count INTEGER,
+    popularity NUMERIC(10, 2),
+    original_language VARCHAR(10),
+    genre_1 INTEGER,
+    genre_2 INTEGER,
+    genre_3 INTEGER,
+    adult_flag BOOLEAN,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
